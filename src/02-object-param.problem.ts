639a7c3d6d9eae4b76b0ccf7219d49bb7cd1ea8c
Yogi_Hariyani_7292 <hariyaniyogi7292@gmail.com>
2022-08-30 11:17:32 +0530

    ts-09

---
+++ src/02-object-param.problem.ts
@@ -0,0 +1,24 @@
+import { expect, it } from "vitest";
+interface Add {
+  first: number;
+  second: number;
+}
+export const addTwoNumbers = (params:Add):number => {
+  return params.first + params.second;
+};
+
+it("Should add the two numbers together", () => {
+  expect(
+    addTwoNumbers({
+      first: 2,
+      second: 4,
+    }),
+  ).toEqual(6);
+
+  expect(
+    addTwoNumbers({
+      first: 10,
+      second: 20,
+    }),
+  ).toEqual(30);
+});
